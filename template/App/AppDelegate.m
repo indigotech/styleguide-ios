852f5794f00f2a8bb343f1a439f21722de1f64e5
Guilherme Hashioka <guilherme.hashioka@gmail.com>
2015-05-20 01:05:30 -0300

    using title stylesheet

--- template/App/AppDelegate.m
+++ template/App/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "HomeViewController.h"
 
 @interface AppDelegate ()
 
@@ -16,7 +17,11 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
+    HomeViewController *viewController = [[HomeViewController alloc] initWithNibName:nil bundle:nil];
+    self.window.rootViewController = viewController;
+    [self.window makeKeyAndVisible];
     return YES;
 }
 
--- template/Modules/Home/HomeViewController.m
+++ template/Modules/Home/HomeViewController.m
@@ -13,25 +13,18 @@
 @end
 
 @implementation HomeViewController
+{
+    __weak IBOutlet UILabel *_titleLabel;
+}
 
+#pragma mark - ViewController lifecyle
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    [self setup];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark - setup
+-(void)setup {
+    _titleLabel.text = @"My favourite label H2";
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
-
 @end
